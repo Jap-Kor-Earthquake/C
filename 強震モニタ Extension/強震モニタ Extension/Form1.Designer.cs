8294438cd1aa3cc8af529c96fe291de864f9b846
Jap-Kor-Earthquake <kangdooho01@gmail.com>
2020-07-01 23:46:42 +0900

    Add files via upload

---
+++ 強震モニタ Extension/強震モニタ Extension/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class 強震モニタExtension
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(強震モニタExtension));
+            this.web = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // web
+            // 
+            this.web.Location = new System.Drawing.Point(-1, -2);
+            this.web.MinimumSize = new System.Drawing.Size(20, 20);
+            this.web.Name = "web";
+            this.web.Size = new System.Drawing.Size(649, 459);
+            this.web.TabIndex = 0;
+            // 
+            // 強震モニタExtension
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 457);
+            this.Controls.Add(this.web);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "強震モニタExtension";
+            this.Text = "強震モニタExtension";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser web;
+    }
+}
+
---
+++ 強震モニタ Extension/強震モニタ Extension/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class 強震モニタExtension : Form
+    {
+        public 強震モニタExtension()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            web.Navigate("https://kwatch-24h.net/2moni/2sec_alm_2monitw.html#");
+        }
+    }
+}
